0cc106769cb79b0edc0edb013df9f7dc2e2360e9
adam25022 <adamnol2502@gmail.com>
2020-04-22 19:13:20 +0100

    numbers and lines mapped on the top, words mapped on the left.

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -27,7 +27,10 @@ public class Gantt extends PApplet
 
 	public void printTasks()
 	{
-		
+		for(tasks Task : task)
+        {
+            println(Task);
+        }
 	}
 	
 	public void mousePressed()
@@ -44,10 +47,39 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		loadTasks();
+		printTasks();
+		draw();
 	}
 	
 	public void draw()
 	{			
 		background(0);
+		colorMode(HSB);
+		stroke(255);
+		int color=0;
+		int margin =width/25;
+		fill(255);
+		for(int i=0; i<9; i++)
+		{
+			float y = map(i, 0, task.size(), 2 * margin, height - margin);
+			text(task.get(i).getTasks(), margin, y);
+		}
+		for(int i=1 ; i<=30; i++)
+		{
+			float x = map(i-1, 0 , 30, 100, width-50);
+			if(color==1)
+			{
+				stroke(175);
+			}
+			else
+			{
+				stroke(255);
+			}
+			text(i, x-2, 35);
+			line(x, height-50, x, 50);
+			color=color+1;
+			color=color%2;
+		}
 	}
 }
